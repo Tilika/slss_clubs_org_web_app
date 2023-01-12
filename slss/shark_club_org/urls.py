c6d1152ad855b6f40f102e85a699f10901a88ec2
Tilika Kulkarni <tilika.kulkarni@gmail.com>
2023-01-12 09:15:07 -0800

    changed path name

--- slss/shark_club_org/urls.py
+++ slss/shark_club_org/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-   path('index/', views.index, name='main-view') 
+   path('index/', views.index, name='views') 
     # 127.0.0.1:8000/shark_club_org/
 ]
