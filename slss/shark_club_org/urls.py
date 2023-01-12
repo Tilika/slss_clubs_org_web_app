0f93adcd1ab73313ae183ec7061f3df25eff4c0b
Tilika Kulkarni <tilika.kulkarni@gmail.com>
2023-01-12 09:09:21 -0800

    changed url patterns in urls.py

--- slss/shark_club_org/urls.py
+++ slss/shark_club_org/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path(r'^$', views.index, name="index") 
+   path('index/', views.index, name='main-view') 
     # 127.0.0.1:8000/shark_club_org/
 ]
