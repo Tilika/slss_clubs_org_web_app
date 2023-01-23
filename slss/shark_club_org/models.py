40043d3c6a16e9e4513f6f8fb91bd5c546f30672
Tilika Kulkarni <tilika.kulkarni@gmail.com>
2023-01-22 16:48:33 -0800

    changed models

--- slss/shark_club_org/models.py
+++ slss/shark_club_org/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class President(models.Model):
+    name = models.CharField(max_length=100)
+    grade = models.CharField(max_length=100)
+
+class Sponsor(models.Model):
+    name = models.CharField(max_length=100)
+    grade = models.CharField(max_length=100)
 
 class Club(models.Model):
     description = models.CharField(max_length=200)
     club_name = models.CharField(max_length=100)
-    president_name = models.CharField(max_length=50)
-    president_grade = models.IntegerField(default=0)
-    sponsor_name = models.CharField(max_length=50)
-    sponsor_room = models.IntegerField(default=0)
+    president = models.ForeignKey(President, on_delete=models.CASCADE)
+    sponsor = models.ForeignKey(Sponsor, on_delete=models.CASCADE)
     contact = models.EmailField()
     instagram = models.URLField(blank=True, null=True)
     facebook = models.URLField(blank=True, null=True)
