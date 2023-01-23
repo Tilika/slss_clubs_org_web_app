24c462e253291f6e3496d42237d880e313d6e1fa
Tilika Kulkarni <tilika.kulkarni@gmail.com>
2023-01-22 17:22:36 -0800

    added str functions to classes

--- slss/shark_club_org/models.py
+++ slss/shark_club_org/models.py
@@ -5,9 +5,14 @@ class President(models.Model):
     name = models.CharField(max_length=100)
     grade = models.IntegerField(default=0)
 
+    def __str__(self):
+        return (f"{self.name}, grade {self.grade}")
+
 class Sponsor(models.Model):
     name = models.CharField(max_length=100)
     room_number = models.IntegerField(default=0)
+    def __str__(self):
+        return (f"{self.name}, grade {self.room_number}")
 
 class Club(models.Model):
     description = models.CharField(max_length=200)
@@ -18,6 +23,9 @@ class Club(models.Model):
     instagram = models.URLField(blank=True, null=True)
     facebook = models.URLField(blank=True, null=True)
 
+    def __str__(self):
+        return (f"{self.club_name}, president is {self.president.name} in grade {self.president.grade}, sponsor is {self.sponsor.name}")
+
 class Event(models.Model):
     club = models.ForeignKey(Club, on_delete=models.CASCADE)
     event_type = models.CharField(max_length=50)
@@ -27,4 +35,6 @@ class Event(models.Model):
     open_to = models.CharField(max_length=50)
     sign_up = models.TextField()
 
+    def __str__(self):
+        return(f"{self.club.club_name} is hosting a {self.event_type} on {self.date_and_time}")
 
