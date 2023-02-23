2fee252f06837f5900c5594540a6686557a6f9e6
teacherubial <42955945+teacherubial@users.noreply.github.com>
2023-02-23 09:47:28 -0800

    Views and Urls todo

--- slss/shark_club_org/views.py
+++ slss/shark_club_org/views.py
@@ -6,3 +6,15 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+# TODO: Events view when student is logged in
+#       All events
+#       Filters
+def event_view(request):
+    # Get all future events
+    # Create the html
+    # return the http response
+
+    return None
+
+
+
